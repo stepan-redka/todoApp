6ab2bbb0861964c735da5c882d71e53d685093f1
Stepan Redka <stepanredka17@gmail.com>
2026-06-02 01:18:27 +0300

    feat: implement Dapper TodoRepository and update database schema seeder with Priority

--- Todo.Infrastructure/DbInitializer.cs
+++ Todo.Infrastructure/DbInitializer.cs
@@ -1,59 +1,72 @@
-﻿using Dapper;                                                                                                                                                                
-    using Microsoft.Data.SqlClient;                                                                                                                                              
-                                                                                                                                                                                 
-    namespace Todo.Infrastructure.Data;                                                                                                                                          
-                                                                                                                                                                                 
-    public class DbInitializer                                                                                                                                                   
-    {                                                                                                                                                                            
-        private readonly string _connectionString;                                                                                                                               
-                                                                                                                                                                                 
-        public DbInitializer(string connectionString)                                                                                                                            
-        {                                                                                                                                                                        
-            _connectionString = connectionString;                                                                                                                                
-        }                                                                                                                                                                        
-                                                                                                                                                                                 
-        public void Initialize()                                                                                                                                                 
-        {                                                                                                                                                                        
-            // 1. Create the database if it doesn't exist                                                                                                                        
-            var builder = new SqlConnectionStringBuilder(_connectionString);                                                                                                     
-            var targetDatabase = builder.InitialCatalog;                                                                                                                         
-                                                                                                                                                                                 
-            // Connect to 'master' database first to run the database creation query                                                                                             
-            builder.InitialCatalog = "master";                                                                                                                                   
-            using (var masterConnection = new SqlConnection(builder.ConnectionString))                                                                                           
-            {                                                                                                                                                                    
-                masterConnection.Open();                                                                                                                                         
-                                                                                                                                                                                 
-                var checkDbQuery = "SELECT database_id FROM sys.databases WHERE name = @DbName";                                                                                 
-                var dbId = masterConnection.ExecuteScalar<int?>(checkDbQuery, new { DbName = targetDatabase });                                                                  
-                                                                                                                                                                                 
-                if (dbId == null)                                                                                                                                                
-                {                                                                                                                                                                
-                    // Create the database                                                                                                                                       
-                    masterConnection.Execute($"CREATE DATABASE [{targetDatabase}]");                                                                                             
-                }                                                                                                                                                                
-            }                                                                                                                                                                    
-                                                                                                                                                                                 
-            // 2. Create the table in our target database if it doesn't exist                                                                                                    
-            using (var connection = new SqlConnection(_connectionString))                                                                                                        
-            {                                                                                                                                                                    
-                connection.Open();                                                                                                                                               
-                                                                                                                                                                                 
-                var createTableSql = @"                                                                                                                                          
-                    IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Todos]') AND type in (N'U'))                                                   
-                    BEGIN                                                                                                                                                        
-                        CREATE TABLE [dbo].[Todos] (                                                                                                                             
-                            [Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY,                                                                                                          
-                            [Title] NVARCHAR(200) NOT NULL,                                                                                                                      
-                            [Description] NVARCHAR(MAX) NULL,                                                                                                                    
-                            [Status] INT NOT NULL,                                                                                                                               
-                            [Deadline] DATETIME2 NULL,                                                                                                                           
-                            [CreatedAt] DATETIME2 NOT NULL,                                                                                                                      
-                            [UpdatedAt] DATETIME2 NULL                                                                                                                           
-                        )                                                                                                                                                        
-                    END";                                                                                                                                                        
-                                                                                                                                                                                 
-                connection.Execute(createTableSql);                                                                                                                              
-            }                                                                                                                                                                    
-        }                                                                                                                                                                        
-    }                                           
+using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace Todo.Infrastructure.Data;
+
+public class DbInitializer
+{
+    private readonly string _connectionString;
+
+    public DbInitializer(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+
+    public void Initialize()
+    {
+        // 1. Create the database if it doesn't exist
+        var builder = new SqlConnectionStringBuilder(_connectionString);
+        var targetDatabase = builder.InitialCatalog;
+
+        // Connect to 'master' database first to run the database creation query
+        builder.InitialCatalog = "master";
+
+        using (var masterConnection = new SqlConnection(builder.ConnectionString))
+        {
+            masterConnection.Open();
+
+            var checkDbQuery =
+                "SELECT database_id FROM sys.databases WHERE name = @DbName";
+
+            var dbId = masterConnection.ExecuteScalar<int?>(
+                checkDbQuery,
+                new { DbName = targetDatabase });
+
+            if (dbId == null)
+            {
+                // Create the database
+                masterConnection.Execute(
+                    $"CREATE DATABASE [{targetDatabase}]");
+            }
+        }
+
+        // 2. Create the table in our target database if it doesn't exist
+        using (var connection = new SqlConnection(_connectionString))
+        {
+            connection.Open();
+
+            var createTableSql = @"
+                IF NOT EXISTS (
+                    SELECT *
+                    FROM sys.objects
+                    WHERE object_id = OBJECT_ID(N'[dbo].[Todos]')
+                      AND type IN (N'U')
+                )
+                BEGIN
+                    CREATE TABLE [dbo].[Todos]
+                    (
+                        [Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY,
+                        [Title] NVARCHAR(200) NOT NULL,
+                        [Description] NVARCHAR(MAX) NULL,
+                        [Status] INT NOT NULL,
+                        [Priority] INT NOT NULL DEFAULT 0,
+                        [Deadline] DATETIME2 NULL,
+                        [CreatedAt] DATETIME2 NOT NULL,
+                        [UpdatedAt] DATETIME2 NULL
+                    )
+                END";
+
+            connection.Execute(createTableSql);
+        }
+    }
+}
--- Todo.Infrastructure/TodoRepository.cs
+++ Todo.Infrastructure/TodoRepository.cs
@@ -1,6 +1,64 @@
+using Dapper;
+using Microsoft.Data.SqlClient;
+using Todo.Domain.Entities;
+using Todo.Domain.Repositories;
+
 namespace Todo.Infrastructure;
 
-public class TodoRepository
+public class TodoRepository : ITodoRepository
 {
-    
+    private readonly string _connectionString;
+
+    public TodoRepository(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+
+    private SqlConnection GetConnection() => new SqlConnection(_connectionString);
+
+    public async Task<TodoItem?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
+    {
+        using var connection = GetConnection();
+        const string sql = "SELECT * FROM Todos WHERE Id = @Id";
+        return await connection.QueryFirstOrDefaultAsync<TodoItem>(sql, new { Id = id });
+    }
+
+    public async Task<IReadOnlyList<TodoItem>> GetAllAsync(CancellationToken cancellationToken = default)
+    {
+        using var connection = GetConnection();
+        const string sql = "SELECT * FROM Todos";
+        var result = await connection.QueryAsync<TodoItem>(sql);
+        return result.ToList().AsReadOnly();
+    }
+
+    public async Task AddAsync(TodoItem item, CancellationToken cancellationToken = default)
+    {
+        using var connection = GetConnection();
+        const string sql = @"
+            INSERT INTO Todos (Id, Title, Description, Status, Priority, Deadline, CreatedAt, UpdatedAt)
+            VALUES (@Id, @Title, @Description, @Status, @Priority, @Deadline, @CreatedAt, @UpdatedAt)";
+        await connection.ExecuteAsync(sql, item);
+    }
+
+    public async Task UpdateAsync(TodoItem item, CancellationToken cancellationToken = default)
+    {
+        using var connection = GetConnection();
+        const string sql = @"
+            UPDATE Todos
+            SET Title = @Title,
+                Description = @Description,
+                Status = @Status,
+                Priority = @Priority,
+                Deadline = @Deadline,
+                UpdatedAt = @UpdatedAt
+            WHERE Id = @Id";
+        await connection.ExecuteAsync(sql, item);
+    }
+
+    public async Task DeleteAsync(Guid id, CancellationToken cancellationToken = default)
+    {
+        using var connection = GetConnection();
+        const string sql = "DELETE FROM Todos WHERE Id = @Id";
+        await connection.ExecuteAsync(sql, new { Id = id });
+    }
 }
